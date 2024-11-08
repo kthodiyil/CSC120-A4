9e0d3519f9f7442e91d5650073869686314b2696
Kavitha Thodiyil <kthodiyil@smith.edu>
2024-11-08 14:22:25 -0500

    Level 1

--- Engine.java
+++ Engine.java
@@ -1,3 +1,49 @@
+import javax.management.RuntimeErrorException;
+
 public class Engine {
+    private FuelType fuelType;
+    private double currentFuelLevel;
+    private double maxFuelLevel;
+
+    public Engine(FuelType fuelType, double currentFuelLevel, double maxFuelLevel){
+        this.fuelType = fuelType;
+        this.currentFuelLevel = currentFuelLevel;
+        this.maxFuelLevel = maxFuelLevel;
+    }
+    public Engine(FuelType fuelType, double fuelLevel){
+        this(fuelType,fuelLevel,fuelLevel);
+    }
+
+    public void refuel(){
+        this.currentFuelLevel = this.maxFuelLevel;
+    }
+
+    public FuelType getFuelType(){
+        return this.fuelType;
+    }
+
+    public boolean go(){
+        if (currentFuelLevel - 1 >= 0.0){
+            this.currentFuelLevel -= 1; // there is enough fuel to decrease by 1
+            System.out.println("Remaining fuel level: " + currentFuelLevel);
+            return true;
+        }
+        else{
+            throw new RuntimeException("Insufficient fuel level"); // not having return false is ok
+        }
+    
+        
+    }
+    public static void main(String[] args) {
+        Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0);
+        while (myEngine.go()) {
+            System.out.println("Choo choo!");
+        }
+        System.out.println("Out of fuel.");
+
+    
+
+
+
 
 }
